723c96da02785bda279cb8f8bfab4cbfff125884
wa7321666-glitch <wa7321666@gmail.com>
2026-01-20 16:02:00 +0800

    project start

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float jumpForce; // jump loncat
+    float score;
+
+    public Text scoretxt; // teks score
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); // perkenalkan inisialisasi
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoretxt.text = "Score: " + score;
+
+        if (Input.GetMouseButtonDown(0)) // klik mouse kiri untuk PC / tap untuk HP
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "point")
+        {
+            score++;
+        }
+        if(collision.gameObject.tag =="pipa")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/spawnScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnScript : MonoBehaviour
+{
+    float time = 0;
+    float timer = 1;
+    public GameObject pipes;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time <= 0)
+        {
+            Instantiate(pipes, transform.position, Quaternion.identity);
+            time = timer;
+        }
+        else
+        {
+            time -= Time.deltaTime;
+        }
+    }
+}
